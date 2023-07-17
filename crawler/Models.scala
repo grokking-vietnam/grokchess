ef08aacd13dd3025911c71c1cd6665e4a3f233a1
Thanh Le <lenguyenthanh@hotmail.com>
2023-07-17 22:31:38 +0200

    Able to send game to Kafka

--- crawler/Models.scala
+++ crawler/Models.scala
@@ -1,7 +1,12 @@
+package grok
+
+import io.circe.Codec
+import io.circe.generic.auto.*
+
 case class Game(
     id: String,
     white: String,
     black: String,
-    winner: Boolean, //true => white, false => black
-    playedAt: String,
-)
+    winner: Boolean, // true => white, false => black
+    playedAt: String
+) derives Codec.AsObject
--- crawler/Producer.scala
+++ crawler/Producer.scala
@@ -1,26 +1,29 @@
-import cats.effect.{ IO, IOApp }
-import fs2.kafka.*
-import scala.concurrent.duration.*
+package grok
+
+import cats.effect.{ IO, IOApp, Resource }
 import fs2.{ Pipe, Stream }
+import scala.concurrent.duration.*
+import io.circe.syntax.*
+import fs2.kafka.*
 
-object Producer:
+object Producer extends IOApp.Simple:
 
-  val server = "localhost:9094"
-  val topic = "lichess-games"
+  val server = "http://localhost:9094"
+  val topic  = "lichess-games1"
 
   val producerSettings =
     ProducerSettings[IO, String, String]
       .withBootstrapServers(server)
 
-  def run: IO[Unit] =
-    val seedStream: Stream[cats.effect.IO, ProducerRecords[String, String]] =
-      Stream
-        .emit(ProducerRecords.one(ProducerRecord(topic, "ping", "pong")))
-        .covary[IO]
+  val seedStream: Stream[cats.effect.IO, ProducerRecords[String, String]] =
+    Stream
+      .emit(ProducerRecords.one(ProducerRecord(topic, "g1", Game("1", "2", "3", true, "4").asJson.noSpaces)))
+      .covary[IO]
 
-    val stream = seedStream
-      .delayBy(1.second)
-      .evalTap(IO.println)
-      .through(KafkaProducer.pipe(producerSettings))
+  val stream = seedStream
+    .evalTap(IO.println)
+    .through(KafkaProducer.pipe(producerSettings))
 
-    IO.println("hello kafka") >> stream.compile.drain
+  IO.println("hello kafka") >> stream.compile.drain
+
+  def run: IO[Unit] = IO.println("hello kafka") >> stream.compile.drain

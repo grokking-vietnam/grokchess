2b6344f9fbfb15201bd985fbbcc4617b6719315d
Bao Tran <nguyenbao1003@hotmail.com>
2023-08-19 21:00:55 -0500

    WIP UI migration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import {
   MDBContainer,
   MDBNavbar,
   MDBNavbarNav,
   MDBNavbarItem,
 } from "mdb-react-ui-kit";
+import { AppShell, Navbar, Text, MediaQuery, Aside, Footer, Header, Burger, Container, useMantineTheme } from '@mantine/core';
 import { Outlet, Routes, Route, Link } from "react-router-dom";
 import { Grokchess } from "./components/Grokchess";
 
 function App() {
+  return <AppShellDemo />;
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
@@ -43,22 +46,71 @@ function Layout() {
     </MDBContainer>
   );
 }
+
+function AppShellDemo() {
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
+  return <AppShell styles={{
+    main: {
+      background: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
+    },
+  }}
+    navbarOffsetBreakpoint="sm"
+    asideOffsetBreakpoint="sm"
+    navbar={
+      <Navbar p="md" hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 200, lg: 300 }}>
+        <Text>Application navbar</Text>
+      </Navbar>
+    }
+    aside={
+      <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
+        <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
+          <Text>Application sidebar</Text>
+        </Aside>
+      </MediaQuery>
+    }
+    footer={
+      <Footer height={60} p="md">
+        Application footer
+      </Footer>
+    }
+    header={
+      <Header height={{ base: 50, md: 70 }} p="md">
+        <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+          <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+            <Burger
+              opened={opened}
+              onClick={() => setOpened((o) => !o)}
+              size="sm"
+              color={theme.colors.gray[6]}
+              mr="xl"
+            />
+          </MediaQuery>
+
+          <Text>Application header</Text>
+        </div>
+      </Header>
+    }
+  >
+    <Text>Resize app to see responsive navbar in action</Text>
+  </AppShell>
+}
 function Home() {
   return (
-    <MDBContainer>
+    <Container>
       <h2>Welcome to Grokking Lab</h2>
-    </MDBContainer>
+    </Container>
   );
 }
 
 function NoMatch() {
   return (
-    <MDBContainer>
+    <Container>
       <h2>Nothing to see here!</h2>
       <p>
         <Link to="/">Go to the home page</Link>
       </p>
-    </MDBContainer>
+    </Container>
   );
 }
 export default App;
